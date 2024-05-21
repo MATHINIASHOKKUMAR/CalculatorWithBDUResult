7b38ab6947d8a5c194f3950b3c7dfe4284c38eb8
Mathini <Mathini@INBOOK_X1_SLIM>
2024-05-21 14:05:28 +0530

    Add project files.

---
+++ CalculatorWithBDUResult/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CalculatorWithBDUResult.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CalculatorWithBDUResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+        public IActionResult Result()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CalculatorWithBDUResult/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalculatorWithBDUResult.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
